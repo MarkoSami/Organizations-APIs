9d8503ac397460fd3a941aa61995fb54f3d52323
marko sami <markohy92018@gmail.com>
2024-10-25 15:24:25 +0300

    Added User schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
-    })
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],

e868b85c560eeabcd462c21fd930bf90ae4c610d
marko sami <markohy92018@gmail.com>
2024-10-25 15:29:43 +0300

    Moved User schema to users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],

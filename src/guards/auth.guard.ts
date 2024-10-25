43af0da58710d4b0f44962a51a0f747858cf8bb1
marko sami <markohy92018@gmail.com>
2024-10-25 19:05:34 +0300

    Added auth guard

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,33 @@
+import { CanActivate, Injectable, Logger, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Request } from "express";
+
+
+
+@Injectable()
+export class AUthGuard implements CanActivate{
+    
+    constructor(private readonly jwtService: JwtService) {}
+    
+    canActivate(context: any): boolean {
+        const request = context.switchToHttp().getRequest();
+        const token = this.extractToken(request);
+        if(!token){
+            throw new UnauthorizedException('Missing token');
+        }
+        try {
+            const decoded = this.jwtService.verify(token);
+            request.userId = decoded.sub;
+        } catch (error) {
+            Logger.error(error);
+            throw new UnauthorizedException('Invalid token');
+        }
+        return true;
+    }
+
+    private extractToken(request: Request): string {
+        const bearerToken = request.headers.authorization.split(' ')[1];
+        return bearerToken;
+    }
+    
+}
